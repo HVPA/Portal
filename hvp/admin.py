034fc7c2794fe9e18734fcc8f31693fc0dd5ca44
Melvin Luong <mel@grattan-dhcp-115.in.vpac.org>
2015-05-27 11:27:11 +1000

    Added most of the models to the django admin. Lab info is not include as that should be kept in a separate database.

--- hvp/admin.py
+++ hvp/admin.py
@@ -13,21 +13,41 @@
 #
 ################################################################################
 
-from Portal.hvp.models import ref 
+from Portal.hvp.models import ref, users, search, labs
 from django.contrib import admin
 
-#class RefSequenceTypeAdmin (admin.ModelAdmin):
-#    list_display = ('SequenceType',)
+# Ref models
+admin.site.register(ref.RefAccessStatus)
+admin.site.register(ref.RefAllele)
+admin.site.register(ref.RefDataType)
+admin.site.register(ref.RefEthnicity)
+admin.site.register(ref.RefLabRequestStatus)
+admin.site.register(ref.RefPathogenicity)
+admin.site.register(ref.RefPhenotypeFeature)
+admin.site.register(ref.RefPhenotypeMethod)
+admin.site.register(ref.RefSampleSource)
+admin.site.register(ref.RefSampleTissue)
+admin.site.register(ref.RefState)
+admin.site.register(ref.RefTestMethod)
+admin.site.register(ref.RefTitle)
+admin.site.register(ref.RefUsageIntention)
+admin.site.register(ref.RefVariantClass)
 
-#admin.site.register(ref.RefSequenceType, RefSequenceTypeAdmin)
-#admin.site.register(ref.RefVariantClass)
-#admin.site.register(ref.RefTestMethod)
-#admin.site.register(ref.RefPathogenicity)
-#admin.site.register(ref.RefSampleTissue)
-#admin.site.register(ref.RefSampleSource)
-#admin.site.register(ref.RefPhenotypeFeature)
-#admin.site.register(ref.RefPhenotypeMethod)
-#admin.site.register(ref.RefEthnicity)
-#admin.site.register(ref.RefUsageIntention)
-#admin.site.register(ref.RefState)
-#admin.site.register(ref.RefTitle)
+# User models
+admin.site.register(users.Consensus)
+admin.site.register(users.InstitutionContact)
+admin.site.register(users.LaboratoryGroup)
+admin.site.register(users.LabRequest)
+admin.site.register(users.UserDocument)
+admin.site.register(users.UserInstitution)
+admin.site.register(users.UserProfile)
+
+# Search models
+admin.site.register(search.Gene)
+admin.site.register(search.GrhaniteHash)
+admin.site.register(search.HG_Build)
+admin.site.register(search.Organisation)
+admin.site.register(search.Patient)
+admin.site.register(search.Phenotype)
+admin.site.register(search.Variant)
+admin.site.register(search.VariantInstance)
